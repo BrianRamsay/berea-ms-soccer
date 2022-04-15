7dc80db9f3f7acac6e291a418e74a0e219e1dd2d
Glitch (glitch-blank-website) <none>
2022-04-15 15:15:22 +0000

    👳❤️ Checkpoint ./index.html:43879872/103 ./js/main.js:43879872/2 ./questions.html:43879872/2436 ./resources.html:43879872/961 ./fields.html:43879872/11 ./css/main.css:43879872/22 ./css/accordion.css:43879872/47 ./js/fields.js:43879872/18

--- js/fields.js
+++ js/fields.js
@@ -1,2 +1,2 @@
-let accordion = new Collapse(document.querySelector(".ours.collapse"), { accordion: false }).init();
-let accordion2 = new Collapse(document.querySelector(".theirs.collapse"), { accordion: false }).init();
+let accordion = new Collapse(document.querySelector(".ours.collapse"), { accordion: true }).init();
+let accordion2 = new Collapse(document.querySelector(".theirs.collapse"), { accordion: true }).init();
