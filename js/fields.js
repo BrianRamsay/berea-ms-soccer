2b6f3ab305b1b56a7aeac22f2a1adf9fdd9f7ed9
Glitch (glitch-blank-website) <none>
2022-04-15 19:20:40 +0000

    👔👀 Checkpoint ./js/fields.js:43879872/891 ./.glitch-assets:43879872/862 ./css/main.css:43879872/462

--- js/fields.js
+++ js/fields.js
@@ -3,10 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
   let accordion2 = new Collapse(document.querySelector(".theirs.collapse"), { accordion: false }).init();
    
   //Convert address tags to google map links - Michael Jasper 2012
-  // image from https://www.flaticon.com/free-icons/google-maps" title="google maps icons">Google maps icons created by justicon - Flaticon</a>
-
-   document.querySelectorAll('address').forEach(function (el) {
-      var link = "<a href='http://maps.google.com/maps?q=" + encodeURIComponent( el.textContent ) + "' target='_blank'>" + el.innerHTML + "</a>";
-      el.innerHTML = link;
-   });
+  // image from https://www.flaticon.com/free-icons/google-maps created by justicon - Flaticon
+  document.querySelectorAll('address').forEach(function (el) {
+    let image = '<img src="https://cdn.glitch.global/29be5113-ffc2-4065-907f-499e5f550584/google-maps icon.png?v=1650049842723" alt="Map this address" />';
+    let linkInner =  image + "<span>" + el.innerHTML + "</span>";
+    let link = "<a href='http://maps.google.com/maps?q=" + encodeURIComponent( el.textContent ) + "' target='_blank'>" + linkInner + "</a>";
+    el.innerHTML = link;
+  });
 });
