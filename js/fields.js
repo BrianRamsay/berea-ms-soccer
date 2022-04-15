cb7a1b989d42ab7026d3059af215f21c89c99866
Glitch (glitch-blank-website) <none>
2022-04-15 18:56:39 +0000

    👏😚 Checkpoint ./fields.html:43879872/910 ./js/fields.js:43879872/787

--- js/fields.js
+++ js/fields.js
@@ -1,2 +1,10 @@
-let accordion = new Collapse(document.querySelector(".ours.collapse"), { accordion: false }).init();
-let accordion2 = new Collapse(document.querySelector(".theirs.collapse"), { accordion: false }).init();
+$(document).ready(function () {
+  let accordion = new Collapse(document.querySelector(".ours.collapse"), { accordion: false }).init();
+  let accordion2 = new Collapse(document.querySelector(".theirs.collapse"), { accordion: false }).init();
+   
+  //Convert address tags to google map links - Michael Jasper 2012
+   document.querySelector('address').each(function () {
+      var link = "<a href='http://maps.google.com/maps?q=" + encodeURIComponent( $(this).text() ) + "' target='_blank'>" + $(this).text() + "</a>";
+      document.querySelector(this).html(link);
+   });
+});
