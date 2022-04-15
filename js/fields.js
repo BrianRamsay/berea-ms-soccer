7403b3c1f8c3618322b2796c8456a9293103548d
Glitch (glitch-blank-website) <none>
2022-04-15 19:08:41 +0000

    🚵👨‍👨‍👦 Checkpoint ./js/fields.js:43879872/491 ./css/main.css:43879872/34 ./fields.html:43879872/21 ./.glitch-assets:43879872/390

--- js/fields.js
+++ js/fields.js
@@ -1,10 +1,12 @@
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function () {
   let accordion = new Collapse(document.querySelector(".ours.collapse"), { accordion: false }).init();
   let accordion2 = new Collapse(document.querySelector(".theirs.collapse"), { accordion: false }).init();
    
   //Convert address tags to google map links - Michael Jasper 2012
-   document.querySelector('address').each(function () {
-      var link = "<a href='http://maps.google.com/maps?q=" + encodeURIComponent( $(this).text() ) + "' target='_blank'>" + $(this).text() + "</a>";
-      document.querySelector(this).html(link);
+  // image from https://www.flaticon.com/free-icons/google-maps" title="google maps icons">Google maps icons created by justicon - Flaticon</a>
+
+   document.querySelectorAll('address').forEach(function (el) {
+      var link = "<a href='http://maps.google.com/maps?q=" + encodeURIComponent( el.textContent ) + "' target='_blank'>" + el.innerHTML + "</a>";
+      el.innerHTML = link;
    });
 });
