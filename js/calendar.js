1447bae1b9d8bf7a4862526293204745a48ec414
Glitch (glitch-blank-website) <none>
2023-08-02 18:00:24 +0000

    🌷🏍 Checkpoint ./calendar.html:43879872/9 ./js/calendar.js:43879872/556

--- js/calendar.js
+++ js/calendar.js
@@ -2,9 +2,17 @@
     to your site with Javascript */
 
 //console.log("Hello, world!");
-document.addEventListener("ready", function(){
+document.addEventListener("DOMContentLoaded", function(){
 
   document.getElementById("calendar-switcher").addEventListener('change',function() {
-    console.log("test");
+    new_class = this.options[this.selectedIndex].value;
+    
+    // hide all
+    var calendars = document.querySelectorAll('.responsive-iframe-container');
+    calendars.forEach((cal) => cal.style.display = 'none');
+    
+    // show desired
+    calendars = document.querySelectorAll(`.${new_class}`);
+    calendars.forEach((cal) => cal.style.display = 'block');
   });
 });
