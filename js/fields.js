b4d857c441db0be672f2344ae2988d10f61b234f
Brian Ramsay <ramsayb2@berea.edu>
2026-09-18 00:45:44 -0400

    More glitch fixes

--- js/fields.js
+++ js/fields.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   //Convert address tags to google map links - Michael Jasper 2012
   // image from https://www.flaticon.com/free-icons/google-maps created by justicon - Flaticon
   document.querySelectorAll('address').forEach(function (el) {
-    let image = '<img src="https://cdn.glitch.global/29be5113-ffc2-4065-907f-499e5f550584/google-maps icon.png?v=1650049842723" alt="Map this address" />';
+    let image = '<img src="images/googlemaps.svg" alt="Map this address" />';
     let linkInner =  image + "<span>" + el.innerHTML + "</span>";
     let link = "<a href='http://maps.google.com/maps?q=" + encodeURIComponent( el.textContent ) + "' target='_blank'>" + linkInner + "</a>";
     el.innerHTML = link;
